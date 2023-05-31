3fb94d346cfc4f397a9d5e930492c09f68238a37
angelina <111738384+angelinapf@users.noreply.github.com>
2023-05-31 23:05:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,86 @@
+// Skapa variabler/DOM-element
 let activity = document.querySelector("#aktivitet");
 let prioritet = document.querySelector("#prioritet");
 const date = document.querySelector('input[type="date"]');
-let ul = document.querySelector("ul");
+let div = document.querySelector("div");
 
+// Skapa en tom array för att lagra todo-uppgifter
 let array = [];
 
+// Funktion för att lägga till en ny todo-uppgift
 function add() {
+  // Skapa ett nytt todo-objekt
   let todo = {
     activity: activity.value,
     prioritet: prioritet.value,
     date: date.value,
   };
 
+  // Lägg till todo-objektet i arrayen
   array.push(todo);
 
-  ul.innerHTML = "";
+  // Rensa div-elementet
+  div.innerHTML = "";
+
+  // Loopa igenom arrayen och skapa p-element för varje todo-uppgift
   for (let i = 0; i < array.length; i++) {
-    let li = document.createElement("li");
-    li.textContent =
-      "activity:" +
+    let h3 = document.createElement("h3");
+    h3.textContent =
       array[i].activity +
-      "\n" +
-      "prioritet:" +
+      "\n";
+
+      let p1 = document.createElement("p");
+      p1.textContent =
       array[i].prioritet +
-      "\n" +
-      " datum:" +
+      "\n";
+      
+      let p2 = document.createElement("p");
+      p2.textContent =
+      "Datum:" +
       array[i].date;
-    ul.appendChild(li);
+
+
+    div.appendChild(h3);
+    div.appendChild(p1);
+    div.appendChild(p2);
   }
+
+  // Rensa inmatningsfälten
   activity.value = "";
   prioritet.value = "";
   date.value = "";
+
+  // Konvertera arrayen till en string och spara den i localStorage
   let array_text = JSON.stringify(array);
   window.localStorage.setItem("todolist", array_text);
 }
+
+// Funktion för att ladda sidan och visa todo-uppgifter från localStorage
 function sidan_laddas() {
-  let array_text = JSON.stringify(array);
+  // Hämta arrayen från localStorage och parsa den tillbaka till en array
+  let array_text = window.localStorage.getItem("todolist");
   array = JSON.parse(array_text);
-  ul.innerHTML = "";
+
+  // Loopa igenom arrayen och skapa p-element för varje todo-uppgift
   for (let i = 0; i < array.length; i++) {
-    let li = document.createElement("li");
-    li.textContent =
-      "activity: " +
+    let h3 = document.createElement("h3");
+    h3.textContent =
       array[i].activity +
-      "prioritet: " +
+      "\n";
+
+      let p1 = document.createElement("p");
+      p1.textContent =
       array[i].prioritet +
-      "datum: " +
+      "\n";
+      
+      let p2 = document.createElement("p");
+      p2.textContent =
+      "Datum:" +
       array[i].date;
-    ul.appendChild(li.textContent);
+
+
+    div.appendChild(h3);
+    div.appendChild(p1);
+    div.appendChild(p2);
   }
 }
